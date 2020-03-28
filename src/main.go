ef0ff575fdfd969313d5e3813a52a3b64045b94f
Rens <github@rensrooimans.nl>
2020-03-28 18:28:05 +0100

    Add random reason to not go to work as demo

--- src/main.go
+++ src/main.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
 func main() {
+	rand.Seed(time.Now().Unix())
 	http.HandleFunc("/", HelloServer)
 	http.ListenAndServe(":8080", nil)
 }
 
-func HelloServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+func HelloServer(w http.ResponseWriter, _ *http.Request) {
+	reason := getRandomOpener()
+	fmt.Fprintf(w, "%s", reason.content )
 }
+
